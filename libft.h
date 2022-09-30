4dfd9cca44ee401818657a4f180f0f84290c42d3
Cristina Trivino Angulo <ctrivino@c3r3s4.42malaga.com>
2022-09-30 17:01:42 +0200

    Adding atoi function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ctrivino <ctrivino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 20:24:35 by ctrivino          #+#    #+#             */
-/*   Updated: 2022/09/29 12:07:31 by ctrivino         ###   ########.fr       */
+/*   Updated: 2022/09/30 16:05:53 by ctrivino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,6 @@ int				ft_strncmp(const char *s1, const char *s2, size_t n);
 void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 char			*ft_strnstr(const char *haystack, const char *neddle, size_t len);
+int             ft_atoi(const char *s);
 
 #endif
